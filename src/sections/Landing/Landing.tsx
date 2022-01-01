fc1ee9814e6da9544dcbb56d6744b6f771ca1e75
Adrian Ronquillo <ronquiaj@plu.edu>
2022-01-01 17:59:53 -0600

    Can now stop the animation if the client presses a button

--- src/sections/Landing/Landing.tsx
+++ src/sections/Landing/Landing.tsx
@@ -1,21 +1,25 @@
-import React, { FC } from "react";
+import React, { FC, useState } from "react";
 import IconFooter from "./IconFooter/IconFooter";
 import TalkingProfilePicture from "./LandingProfilePicture/TalkingProfilePicture";
 import LandingText from "./LandingText/LandingText";
 
 const Landing: FC = () => {
-  console.log("landing6");
+  const [isDuctTaped, setIsDuctTaped] = useState(false);
   return (
     <section className="flex flex-col items-center w-full min-h-full m-0 font-medium bg-background debug-screens">
       <h1 className="mt-12 tracking-widest text-center pointer-events-none hover:translate-y-4 drop-shadow-md xs:text-4xl sm:text-6xl 3xl:text-7xl text-primary">
         adrian ronquillo
       </h1>
       <TalkingProfilePicture
+        animating={!isDuctTaped}
         interval={5}
-        words={["pst.....", "hey you.....", "yeah you.....", "click me....."]}
+        words={["pst...", "hey you...", "yeah you...", "click me..."]}
       />
       <LandingText />
       <IconFooter className="mt-auto -translate-y-5 icon-footer" />
+      {!isDuctTaped && (
+        <button onClick={() => setIsDuctTaped(true)}>duct tape</button>
+      )}
     </section>
   );
 };
--- src/sections/Landing/LandingProfilePicture/TalkingProfilePicture.tsx
+++ src/sections/Landing/LandingProfilePicture/TalkingProfilePicture.tsx
@@ -2,35 +2,44 @@ import React, { FC, useEffect, useRef } from "react";
 import ProfilePicture from "components/ProfilePicture/ProfilePicture";
 import "./styles.css";
 
-type Props = { interval: number; words: string[]; className?: string };
+type Props = {
+  animating: boolean;
+  interval: number;
+  words: string[];
+  className?: string;
+};
 
 const TalkingProfilePicture: FC<Props> = ({
   className,
   interval,
   words,
+  animating,
 }: Props) => {
   const animationContainerRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
+    let intervalID: NodeJS.Timer;
+    const animationRef = animationContainerRef.current;
+
     const startAnimation = () =>
       words.forEach((word, index) => animateWord(word, interval * index));
 
-    const animateWord = (word: string, timeout: number) => {
+    const animateWord = (word: string, timeout: number) =>
       setTimeout(() => {
-        if (animationContainerRef.current)
-          animationContainerRef.current.innerHTML = `<h3 class='animation-text'>${word}</h3>`;
+        if (animationRef)
+          animationRef.innerHTML = `<h3 class='animation-text'>${word}</h3>`;
       }, timeout * 1000);
+
+    if (animating) {
+      startAnimation(); // Start animation immediately, then start an interval for it
+      intervalID = setInterval(startAnimation, interval * 1000 * words.length);
+    }
+
+    return () => {
+      if (animationRef) animationRef.style.setProperty("visibility", "hidden"); // Immediately stop any text already being shown by hiding parent element
+      clearInterval(intervalID);
     };
-
-    startAnimation(); // Start animation immediately, then start an interval for it
-
-    const intervalID = setInterval(
-      startAnimation,
-      interval * 1000 * words.length
-    );
-
-    return () => clearInterval(intervalID);
-  }, [interval, words]);
+  }, [interval, words, animating]);
 
   return (
     <div className="flex items-center justify-center picture-container">
