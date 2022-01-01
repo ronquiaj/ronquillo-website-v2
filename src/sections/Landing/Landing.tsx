2a812c605fdd5a87a63bb47164e5b4bd393fe0d7
Adrian Ronquillo <ronquiaj@plu.edu>
2022-01-01 17:30:56 -0600

    Animation for talking profile picture works

--- src/sections/Landing/Landing.tsx
+++ src/sections/Landing/Landing.tsx
@@ -4,14 +4,15 @@ import TalkingProfilePicture from "./LandingProfilePicture/TalkingProfilePicture
 import LandingText from "./LandingText/LandingText";
 
 const Landing: FC = () => {
+  console.log("landing6");
   return (
     <section className="flex flex-col items-center w-full min-h-full m-0 font-medium bg-background debug-screens">
       <h1 className="mt-12 tracking-widest text-center pointer-events-none hover:translate-y-4 drop-shadow-md xs:text-4xl sm:text-6xl 3xl:text-7xl text-primary">
         adrian ronquillo
       </h1>
       <TalkingProfilePicture
-        interval={3}
-        words={["pst.....", "click me....."]}
+        interval={5}
+        words={["pst.....", "hey you.....", "yeah you.....", "click me....."]}
       />
       <LandingText />
       <IconFooter className="mt-auto -translate-y-5 icon-footer" />
--- src/sections/Landing/LandingProfilePicture/TalkingProfilePicture.tsx
+++ src/sections/Landing/LandingProfilePicture/TalkingProfilePicture.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from "react";
+import React, { FC, useEffect, useRef } from "react";
 import ProfilePicture from "components/ProfilePicture/ProfilePicture";
 import "./styles.css";
 
@@ -9,13 +9,35 @@ const TalkingProfilePicture: FC<Props> = ({
   interval,
   words,
 }: Props) => {
+  const animationContainerRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const startAnimation = () =>
+      words.forEach((word, index) => animateWord(word, interval * index));
+
+    const animateWord = (word: string, timeout: number) => {
+      setTimeout(() => {
+        if (animationContainerRef.current)
+          animationContainerRef.current.innerHTML = `<h3 class='animation-text'>${word}</h3>`;
+      }, timeout * 1000);
+    };
+
+    startAnimation(); // Start animation immediately, then start an interval for it
+
+    const intervalID = setInterval(
+      startAnimation,
+      interval * 1000 * words.length
+    );
+
+    return () => clearInterval(intervalID);
+  }, [interval, words]);
+
   return (
-    <div className="flex items-center justify-center">
-      <div className="absolute ">
-        <h3 className="relative z-10 3xl:bottom-16 xl:bottom-0 xs:-bottom-3 left-1/2">
-          pst.........
-        </h3>
-      </div>
+    <div className="flex items-center justify-center picture-container">
+      <div
+        ref={animationContainerRef}
+        className="absolute animation-container"
+      ></div>
       <ProfilePicture
         className={`object-cover object-[-12px] xs:mb-8 sm:mb-16 xl:mb-20 3xl:mb-52 ${className}`}
         alt="adrian"
