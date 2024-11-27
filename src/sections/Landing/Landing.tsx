c6b0226bf20aed1584704ab6e650e0ccad9c4c33
ronquiaj <ronquiaj@gmail.com>
2024-11-27 12:53:25 -0800

    Make age dynamic

--- src/sections/Landing/Landing.tsx
+++ src/sections/Landing/Landing.tsx
@@ -4,9 +4,17 @@ import LandingText from "./LandingText/LandingText";
 import LandingProfilePicture from "./LandingProfilePicture/LandingProfilePicture";
 import DuctTape from "./DuctTape/DuctTape";
 
+const BIRTHDAY = new Date("1999-10-26");
+const calculateAge = () => {
+  const diff = Date.now() - BIRTHDAY.getTime();
+  const ageDate = new Date(diff);
+  return Math.abs(ageDate.getUTCFullYear() - 1970);
+};
+
 const Landing: FC = () => {
   const [isDuctTaped, setIsDuctTaped] = useState(false);
   const [stage, setStage] = useState(1);
+  const calculatedAge = calculateAge();
   return (
     <section className="flex flex-col items-center w-full min-h-full m-0 font-medium bg-background">
       <h1 className="mt-12 tracking-widest text-center pointer-events-none hover:translate-y-4 drop-shadow-md xs:text-4xl sm:text-6xl 3xl:text-7xl text-primary">
@@ -18,7 +26,7 @@ const Landing: FC = () => {
         stage={stage}
       />
       <LandingText
-        landingText="san jose, california . 23 years old . actor / developer . filipino"
+        landingText={`san jose, california . ${calculatedAge} years old . actor / developer . filipino`}
         animateText={stage !== 1}
       />
       <IconFooter className="mt-auto -translate-y-5 icon-footer " />
